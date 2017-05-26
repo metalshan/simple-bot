38b763ff4648e68419ca928756ccac95de07174f
Paul <go2mailshan@gmail.com>
2017-05-27 00:10:19 +0530

    refactoring the worker

--- src/Worker.js
+++ src/Worker.js
@@ -5,37 +5,22 @@ let {JSDOM} = require("jsdom");
 let urlUtil = require("./utils/url");
 
 class Worker{
+    //this function does the scraping
     work(url){
-        return new Promise((resolve, reject)=>{
-            request(url, function (error, response, body) {
+        return new Promise(resolve=>{
+            request(url, (error, response, body) => {
                 if(error){
-                    resolve({
-                        status: false,
-                        data: {
-                            url,
-                            title: "Failed to scrape"
-                        },
-                        urls : []
-                    });
+                    resolve(this.failureResponse(url));
                 } else{
-                    const dom = new JSDOM(body);
-                    
+                    const dom = new JSDOM(body);                    
                     //find title
-                    let titleElement = dom.window.document.querySelector("title");
-                    let title = titleElement ? titleElement.textContent : "No title";
-
+                    let title = this.findTitleFromDOM(dom);
                     //findLinks 
-                    let anchorLinks = dom.window.document.querySelectorAll("a");
-                    let urls = [];
-                    Array.from(anchorLinks).forEach(node=>{
-                        let anchorUrl = node.getAttribute("href");
-                        if(urlUtil.isSameDomain(anchorUrl, url)){
-                            urls.push(anchorUrl);
-                        }
-                    });
-                    
+                    let urls = this.findRelativeUrlsFromDOM(dom, url);
+
                     //resolving with appropriate data
                     resolve({
+                        status: true,
                         data:{
                             url,
                             title
@@ -45,6 +30,38 @@ class Worker{
                 }
             });
         });
+    }
+
+    //get the title from the dom element
+    findTitleFromDOM(dom){
+        let titleElement = dom.window.document.querySelector("title");
+        let title = titleElement ? titleElement.textContent : "No title";
+        return title;
+    }
+
+    //get the relative links from the page
+    findRelativeUrlsFromDOM(dom, url){
+        let anchorLinks = dom.window.document.querySelectorAll("a");
+        let urls = [];
+        Array.from(anchorLinks).forEach(node=>{
+            let anchorUrl = node.getAttribute("href");
+            if(urlUtil.isSameDomain(anchorUrl, url)){
+                urls.push(anchorUrl);
+            }
+        });
+        return urls;
+    }
+
+    //failure response
+    failureResponse(url){
+        return {
+            status: false,
+            data: {
+                url,
+                title: "Failed to scrape"
+            },
+            urls : []
+        };
     }
 }
 
