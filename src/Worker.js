18724ca718eb8ce91a35fb1cac4da59f6ffc6959
Paul <go2mailshan@gmail.com>
2017-05-26 21:00:32 +0530

    scraping flow complete

--- src/Worker.js
+++ src/Worker.js
@@ -1,17 +1,42 @@
 "use strict";
 
+let request = require("request");
+let {JSDOM} = require("jsdom");
+let urlUtil = require("./utils/url");
+
 class Worker{
     work(url){
-        return new Promise((resolve)=>{
-            setTimeout(()=>{
-                resolve({
-                    data:{
-                        url: url,
-                        title: "I am a title"
-                    },
-                    urls: ["url1", "url2"]
-                });
-            },2000);
+        return new Promise((resolve, reject)=>{
+            request(url, function (error, response, body) {
+                if(error){
+                    reject(url);
+                } else{
+                    const dom = new JSDOM(body);
+                    
+                    //find title
+                    let titleElement = dom.window.document.querySelector("title");
+                    let title = titleElement ? titleElement.textContent : "No title";
+
+                    //findLinks 
+                    let anchorLinks = dom.window.document.querySelectorAll("a");
+                    let urls = [];
+                    Array.from(anchorLinks).forEach(node=>{
+                        let anchorUrl = node.getAttribute("href");
+                        if(urlUtil.isSameDomain(anchorUrl, url)){
+                            urls.push(anchorUrl);
+                        }
+                    });
+                    console.log(urls)
+                    //resolving with appropriate data
+                    resolve({
+                        data:{
+                            url,
+                            title
+                        },
+                        urls
+                    });
+                }
+            });
         });
     }
 }
